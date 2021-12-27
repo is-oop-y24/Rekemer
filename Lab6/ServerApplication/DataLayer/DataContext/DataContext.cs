a2457caba8b10428b21435df3608b2e359a34a8e
Rekemer <89906591+Rekemer@users.noreply.github.com>
2021-12-27 15:41:09 +0300

    refactoring: refactor main script of server and client

---
+++ Lab6/ServerApplication/DataLayer/DataContext/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ServerApplication.Common.Models;
+using ServerApplication.DataLayer.Repository;
+
+namespace ServerApplication.DataLayer.DataContext
+{
+    public abstract class DataContext
+    {
+        public DataToStore DataToStore { get; set; } = new DataToStore();
+        public abstract void LoadTasks();
+        public abstract void LoadWorkers();
+        public abstract void Save();
+
+        public List<T> Set<T>()
+        {
+            if (typeof(T) == typeof(Worker))
+            {
+                return DataToStore.Workers as List<T>;
+            }
+
+            if (typeof(T) == typeof(Task))
+            {
+                return DataToStore.Tasks as List<T>;
+            }
+
+            return null;
+        }
+    }
+}
